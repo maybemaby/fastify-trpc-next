e2497b5ceb738af17ff2f24be37184c6d44b81b3
maybemaby <74226064+maybemaby@users.noreply.github.com>
2023-01-20 21:21:39 -0800

    Add testing deps

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+/** @type {import("next").NextConfig} */
+const config = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     transpilePackages: ["ui"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(config);
